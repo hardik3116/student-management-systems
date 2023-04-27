5ab35358626ff9cf1b856807a9bcdb312599d55c
hardik3112 <hardikalphabin@gmail.com>
2023-04-27 20:19:55 +0530

    added project

---
+++ student_management_app/migrations/0004_auto_20200703_0740.py
@@ -0,0 +1,18 @@
+
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('student_management_app', '0003_auto_20200627_0534'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='leavereportstudent',
+            name='leave_status',
+            field=models.IntegerField(default=0),
+        ),
+    ]
